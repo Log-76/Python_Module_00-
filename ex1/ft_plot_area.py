15daa501bdf24dc48fc29d45a5a3d5b7f8038127
Logann Leriche <lleriche@c2r12p6.42lehavre.fr>
2026-01-20 12:15:07 +0100

    a

---
+++ ex1/ft_plot_area.py
@@ -0,0 +1,5 @@
+def ft_plot_area():
+    length = int(input('Enter length: '), 10)
+    width = int(input('Enter width: '), 10)
+    area = length * width
+    print('Enter area:', area)
---
+++ ex2/ft_harvest_total.py
@@ -0,0 +1,5 @@
+def ft_harvest_total():
+    harvest = int(input('Day 1 harvest:'), 10)
+    harvest = harvest + int(input('Day 2 harvest:'), 10)
+    harvest = harvest + int(input('Day 3 harvest:'), 10)
+    print('Total harvest:', harvest)
