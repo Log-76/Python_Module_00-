6a9009b50dfddd2a033593e3d798fe554be5c7ec
Logann Leriche <lleriche@c2r12p6.42lehavre.fr>
2026-01-22 15:07:19 +0100

    corr

--- ex7/ft_seed_inventory.py
+++ ex7/ft_seed_inventory.py
@@ -1,3 +1,10 @@
 def ft_seed_inventory(str, num, type):
-    str = str.capitalize()
-    print(str + ' seeds:', num, type)
+    if type == "packets":
+        print(str.capitalize() + ' seeds:', num, type, "available")
+        return
+    if type == "grams":
+        print(str.capitalize() + ' seeds:', num, type, "total")
+        return
+    if type == "area":
+        print(str.capitalize() + ' seeds:', num, "square meters")
+        return
